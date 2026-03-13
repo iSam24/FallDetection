a5ad54c2f706cea083412d8ee01d949500ac5c31
iSam24 <samwhitesammy@live.co.uk>
2026-03-13 21:07:16 +0000

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "main_functions.h"
+
+/**
+* @brief      Arduino setup function
+*/
+void setup()
+{
+    setup_func();
+}
+
+/**
+* @brief      Get data and run inferencing
+*/
+void loop()
+{
+    loop_func();
+}
---
+++ src/main_functions.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_FUNCTIONS_H
+#define _MAIN_FUNCTIONS_H
+
+// Forward declarations
+void setup_func(void);
+void loop_func(void);
+void run_inference_background(bool debug);
+float ei_get_sign(float number);
+
+
+
+#endif // _MAIN_FUNCTIONS_H
